77bf7704145093d6394c72494d6d964d7582d30c
adamascj2 <adamascj@gmail.com>
2025-07-20 15:05:21 -0300

    Add files via upload

---
+++ piscadas.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as gpio
+import time
+continuar = 'S'
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11.GPIO.OUT)
+while continuar = 'S'
+	piscadas = int(input('Quantas piscadas vc deseja? :'))
+	for i range(0,piscadas)
+		GPIO.output(11,ON)
+		time.sleep(1)
+		GPIO.output(11,OFF)
+		time.sleep(1)
+	continuar = input('Quer continuar testando? (S para SIM):')
+GPIO.cleanup()
+
+               
