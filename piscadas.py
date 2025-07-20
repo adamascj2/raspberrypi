ad90c0a49835ca373b5b1921f438bd67b53af278
adamascj2 <adamascj@gmail.com>
2025-07-20 16:59:33 -0300

    Add files via upload

--- piscadas.py
+++ piscadas.py
@@ -6,9 +6,9 @@ GPIO.setup(11,GPIO.OUT)
 while continuar == 'S':
 	piscadas = int(input('Quantas piscadas vc deseja? :'))
 	for i in range(0,piscadas):
-		GPIO.output(11,ON)
+		GPIO.output(11,1)
 		time.sleep(1)
-		GPIO.output(11,OFF)
+		GPIO.output(11,0)
 		time.sleep(1)
 	continuar = input('Quer continuar testando? (S para SIM):')
 GPIO.cleanup()
