2631a0f2162265fff0a6db67a2a6b284f3646167
adamascj2 <adamascj@gmail.com>
2025-07-20 16:39:06 -0300

    Add files via upload

--- piscadas.py
+++ piscadas.py
@@ -3,9 +3,9 @@ import time
 continuar = 'S'
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
-while continuar == 'S'
+while continuar == 'S':
 	piscadas = int(input('Quantas piscadas vc deseja? :'))
-	for i range(0,piscadas)
+	for i range(0,piscadas):
 		GPIO.output(11,ON)
 		time.sleep(1)
 		GPIO.output(11,OFF)
