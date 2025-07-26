8f422be37a6df54d9a2446808f97de08d5cbf1ef
adamascj2 <adamascj@gmail.com>
2025-07-26 16:48:11 -0300

    Add files via upload

---
+++ push1.py
@@ -0,0 +1,19 @@
+import time    
+import RPi.GPIO as GPIO    
+GPIO.setmode(GPIO.BOARD)   
+pinoBotao=13                 
+pinoLED=11                   
+GPIO.setup(pinoBotao,GPIO.IN,pull_up_down=GPIO.PUD_UP)  
+GPIO.setup(pinoLED,GPIO.OUT)
+circuitoAberto=1 
+try: 
+	while True: 
+        	circuitoAberto = GPIO.input(pinoBotao)  # Verifica se circuito aberto ou nao      	
+         	if circuitoAberto==1:
+              		GPIO.output(pinoLED,0)  
+              		time.sleep(.5)
+         	if circuitoAberto==0:        # Botao apertado!
+              		GPIO.output(pinoLED,1)  
+              		time.sleep(.5)
+except KeyboardInterrupt:    # Ctrl c para interromper
+	GPIO.cleanup()
