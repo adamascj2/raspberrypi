4c5be7e1f4356b4f66986aed18ed4a0217cc5c16
adamascj2 <adamascj@gmail.com>
2025-07-28 16:16:46 -0300

    Add files via upload

--- tkinterpush2.py
+++ tkinterpush2.py
@@ -1,37 +1,38 @@
 import tkinter as tk
+import time    
+import RPi.GPIO as GPIO
+    
+GPIO.setmode(GPIO.BOARD)
+pinoLED=11                   
+GPIO.setup(pinoLED,GPIO.OUT)   
 def acende():
-	import time    
-	import RPi.GPIO as GPIO    
-	GPIO.setmode(GPIO.BOARD)   
-    	pinoLED=11                   
-	GPIO.setup(pinoLED,GPIO.OUT)
-	estado=1 
-	estadoAnterior=1
-	estadoLED=0
-	try: 
-		while True: 
-			print(estado)
-			if estado==1 and estadoAnterior==0
-				estadoLED=not estadoLED  #Inverte o estadoLED
-              			GPIO.output(pinoLED,estadoLED)
-				estadoAnterior=estado  
-             		time.sleep(.5)
+	GPIO.output(pinoLED,1)
+	time.sleep(.5)
+def apaga():
+	GPIO.output(pinoLED,0)
+	time.sleep(.5)
 
-	except KeyboardInterrupt:    # Ctrl c para interromper
+try:
+	
+	# Cria a janela principal da aplicação Tkinter
+	app = tk.Tk()
+	app.title("LIGA/DESLIGA")
+	app.geometry("400x250") # Define o tamanho da janela
+
+	# Cria um rótulo (texto)
+	label = tk.Label(app, text="Clique no botão para acender/apagar o LED:", font=("Arial", 12))
+	label.pack(pady=20) # Adiciona o rótulo à janela com espaçamento
+
+	# Cria o botão1
+	button1 = tk.Button(app, text="ACENDER", command=acende, font=("Arial", 14), bg="lightblue")
+	button1.pack(pady=10) # Adiciona o botão à janela com espaçamento
+	# Cria o botão2
+	button2 = tk.Button(app, text="APAGAR", command=apaga, font=("Arial", 14), bg="lightblue")
+	button1.pack(pady=10) # Adiciona o botão à janela com espaçamento
+
+	# Inicia o loop principal da aplicação Tkinter
+	app.mainloop()
+
+except KeyboardInterrupt:    # Ctrl c para interromper
 		GPIO.cleanup()
         	print('Tudo limpo'
-# Cria a janela principal da aplicação Tkinter
-app = tk.Tk()
-app.title("PushButton")
-app.geometry("300x150") # Define o tamanho da janela
-
-# Cria um rótulo (texto)
-label = tk.Label(app, text="Clique no botão para acender/apagar o LED:", font=("Arial", 12))
-label.pack(pady=20) # Adiciona o rótulo à janela com espaçamento
-
-# Cria o botão
-button = tk.Button(app, text="ACENDER/APAGAR", command=acende, font=("Arial", 14), bg="lightblue")
-button.pack(pady=10) # Adiciona o botão à janela com espaçamento
-
-# Inicia o loop principal da aplicação Tkinter
-app.mainloop()
