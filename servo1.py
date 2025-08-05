4f007a1904004e7287e233b1c65f7734ef51fe02
adamascj2 <adamascj@gmail.com>
2025-08-05 10:44:46 -0300

    Add files via upload

--- servo1.py
+++ servo1.py
@@ -3,17 +3,17 @@ import time
 GPIO.setmode(GPIO.BOARD)   
 pinoPWM=12
 GPIO.setup(pinoPWM,GPIO.OUT)
-oPWM = GPIO.PWM(pinoLED,50)   #Tem que ser 50 hertz/seg
+oPWM = GPIO.PWM(pinoPWM,50)   #Tem que ser 50 hertz/seg
 DC=0   #Definição da variavel com valor do Dutycycle
 oPWM.start(DC)
 
 try:
-	while True:
-       		DC = float('Entre com um DC entre 0 e 15: ')	
-		oPWM.ChangeDutyCycle(DC)	
-		time.sleep(3)
-		 
+    while True:
+        DC = float (input('Entre com um DC entre 4 e 10 ')) #7 é horizontal
+        oPWM.ChangeDutyCycle(DC)	
+        time.sleep(3)
+         
 except KeyboardInterrupt:    # Ctrl c para interromper
-        oPWM.stop()
-	GPIO.cleanup()
-        print('Tudo limpo')
+    oPWM.stop()
+    GPIO.cleanup()
+    print('Tudo limpo')
