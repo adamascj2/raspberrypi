bc6e2fe6d09b4adfa5fc924543147c869433054e
adamascj2 <adamascj@gmail.com>
2025-08-09 15:10:46 -0300

    Add files via upload

---
+++ colisao1.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time    
+GPIO.setmode(GPIO.BOARD) 
+pinoTRIG = 16
+pinoECHO = 18
+GPIO.setup(pinoTRIG,GPIO.OUT)
+GPIO.setup(pinoECHO,GPIO.IN)
+
+try:
+    while True:
+	GPIO.output(pinoTRG,0)
+	time.sleep(2E-6)        #2 mSeg
+        GPIO.output(pinoTRG,1)
+	time.sleep(10E-6)       #10 mSeg
+        GPIO.output(pinoTRG,0)
+	while GPIO.input(pinoECHO)==0
+		pass
+	inicioT=time.time()
+	while GPIO.input(pinoECHO)==1
+		pass
+	fimT=time.time()
+	T = fimT-inicioT
+	print('T: ',T*1E6,' DISTANCIA: ',T*343/2)
+
+
+
+ 
+except KeyboardInterrupt:    # Ctrl c para interromper
+    GPIO.cleanup()
+    print('Tudo limpo')
