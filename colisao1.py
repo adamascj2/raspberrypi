6165e5802bb98262b62219b673016a88974a1154
adamascj2 <adamascj@gmail.com>
2025-08-09 17:09:24 -0300

    Add files via upload

--- colisao1.py
+++ colisao1.py
@@ -8,23 +8,20 @@ GPIO.setup(pinoECHO,GPIO.IN)
 
 try:
     while True:
-	GPIO.output(pinoTRG,0)
-	time.sleep(2E-6)        #2 mSeg
-        GPIO.output(pinoTRG,1)
-	time.sleep(10E-6)       #10 mSeg
-        GPIO.output(pinoTRG,0)
-	while GPIO.input(pinoECHO)==0
-		pass
-	inicioT=time.time()
-	while GPIO.input(pinoECHO)==1
-		pass
-	fimT=time.time()
-	T = fimT-inicioT
-	print('T: ',T*1E6,' DISTANCIA: ',T*343/2)
+        GPIO.output(pinoTRIG,0)
+        time.sleep(2E-6)        #2 mSeg
+        GPIO.output(pinoTRIG,1)
+        time.sleep(10E-6)       #10 mSeg
+        GPIO.output(pinoTRIG,0)
+        while GPIO.input(pinoECHO)==0:
+            pass
+        inicioT=time.time()
+        while GPIO.input(pinoECHO)==1:
+            pass
+        fimT=time.time()
+        T = fimT-inicioT
+        print('T: ',T*1E6,' DISTANCIA: ',int(T*343/2*100 +0.5 ))
 
-
-
- 
 except KeyboardInterrupt:    # Ctrl c para interromper
     GPIO.cleanup()
     print('Tudo limpo')
