6abfd3fad4fbfcfc86e40d300c75fcea0fd68515
adamascj2 <adamascj@gmail.com>
2025-08-18 09:36:41 -0300

    Update parede1.py

--- parede1.py
+++ parede1.py
@@ -1,26 +1,9 @@
  
-from robot_hat import Ultrasonic, Pin
-from robot_hat import Motor, PWM, Pin
-us = Ultrasonic(Pin("D2"), Pin("D3"))
-motor = Motor(PWM("P13"), Pin("D4"))try:
-	 
-	motor.speed(20)
-	 
- 
-	while True:
- 		# Read distance
-		distance = us.read()
-		print(f"Distance: {distance}cm")
-		if distance >= 5:
-			# Stop all motors
-			motor.speed(0)
 
-			break
-
-except KeyboardInterrupt:    # Ctrl c para interromper
 	print('Encerrado')
 
  
 
  
+
 
